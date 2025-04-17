1c6879fdd8752c5e26bfb6c77a5f4967ad5fc3ef
Tand00ru <119233752+Tand00ru@users.noreply.github.com>
2025-04-17 21:32:11 +0300

    first commit

--- static/js/cart.js
+++ static/js/cart.js
@@ -5,16 +5,14 @@ document.addEventListener("DOMContentLoaded", function () {
         const inputField = cart.querySelector(".quantity_add_to_cart");
 
         function updateValue(change) {
-            let currentValue = parseInt(inputField.value) || 1; // Если пусто, ставим 1
+            let currentValue = parseInt(inputField.value) || 1;
             let newValue = currentValue + change;
 
-            // Ограничиваем в диапазоне от 1 до 100
             if (newValue >= 1 && newValue <= 100) {
                 inputField.value = newValue;
             }
         }
 
-        // Обработчики кнопок
         plusButton.addEventListener("click", function () {
             updateValue(1);
         });
@@ -23,16 +21,13 @@ document.addEventListener("DOMContentLoaded", function () {
             updateValue(-1);
         });
 
-        // Ограничение ввода только числами и проверка на max
         inputField.addEventListener("input", function () {
-            this.value = this.value.replace(/\D/g, ""); // Убираем буквы
+            this.value = this.value.replace(/\D/g, "");
             let value = parseInt(this.value);
 
-            // Если введено меньше 1 — ставим 1
             if (isNaN(value) || value < 1) {
                 this.value = 1;
             }
-            // Если введено больше 100 — ставим 100
             else if (value > 100) {
                 this.value = 100;
             }
